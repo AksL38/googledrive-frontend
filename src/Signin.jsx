ae106c68c7eda72fc0f15bad0af6fd07861ea330
AksL38 <46625619+AksL38@users.noreply.github.com>
2021-09-26 14:30:56 +0530

    signin route

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -1,3 +1,51 @@
+import axios from "axios";
+import { Formik, Field, Form } from "formik";
+import { useRef } from "react";
+
 export default function Signin() {
-  return <></>;
+  const loggedStatus = useRef(null);
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={async ({ email, password }) => {
+          try {
+            const response = await axios.post(
+              "https://googledrive-backend-aksl38.herokuapp.com/auth/signin",
+              { email, password }
+            );
+
+            if (response.status === 200) {
+              loggedStatus.current.textContent = response.data.message;
+            } else {
+              loggedStatus.current.textContent =
+                "Something went wrong. All fields are required. " +
+                response.data.error;
+            }
+          } catch (error) {
+            loggedStatus.current.textContent =
+              "Something went wrong. All fields are required. " + error.message;
+          }
+        }}
+      >
+        <Form>
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="john@example.com"
+            type="email"
+          />
+          <label htmlFor="password">Password</label>
+          <Field id="password" name="password" type="password" />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+      <p ref={loggedStatus}></p>
+    </div>
+  );
 }
